a9f7ba9dbc8c4eb7cd09037074150ab02543e373
Rafael <RafaelB24@yahoo.com>
2020-03-29 12:29:01 -0700

    Add tests to check if a user is created with default values

--- bg-meetups-back-end/spec/models/user_spec.rb
+++ bg-meetups-back-end/spec/models/user_spec.rb
@@ -4,8 +4,20 @@ RSpec.describe User, type: :model do
   context 'with correct attributes' do
     describe 'it can be created with correct attributes' do
       it 'can be created successfully' do 
-        user = User.new(name: 'test', email: 'test@email.com', interest: 'test', whitebox: [], blackbox: [], default_blackbox_message: 'this is a test', password: 'test123')
+        user = User.new(name: 'test', email: 'test@email.com', interest: 'test', whitebox: [], blackbox: [], password: 'test123')
         expect(user.save).to be_truthy
+      end 
+      it 'is created with the correct default values' do
+        user = User.new(name: 'test', email: 'test@email.com', interest: 'test', whitebox: [], blackbox: [], password: 'test123')
+        user.save 
+
+        expect(user.whitebox.class).to eq(Array)
+        expect(user.blackbox.class).to eq(Array)
+        expect(user.vip).to be_falsey
+        expect(user.default_blackbox_message).to eq('User has denied the invitation')
+        expect(user.approved).to eq(1)
+        expect(user.appealed).to eq(1)
+        expect(user.access).to eq(2)
       end 
     end 
   end   
