095ee91997cdcb2329d44059dc9d9152c403bd38
Rafael <RafaelB24@yahoo.com>
2020-03-29 13:09:36 -0700

    Add before context hook that creates a user

--- bg-meetups-back-end/spec/models/user_spec.rb
+++ bg-meetups-back-end/spec/models/user_spec.rb
@@ -2,22 +2,21 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'with correct attributes' do
+    before(:context) do 
+      @user = User.create(name: 'test', email: 'test@email.com', interest: 'test', whitebox: [], blackbox: [], password: 'test123')
+    end 
     describe 'it can be created with correct attributes' do
       it 'can be created successfully' do 
-        user = User.new(name: 'test', email: 'test@email.com', interest: 'test', whitebox: [], blackbox: [], password: 'test123')
-        expect(user.save).to be_truthy
+        expect(@user.errors.any?).to be_falsey
       end 
       it 'is created with the correct default values' do
-        user = User.new(name: 'test', email: 'test@email.com', interest: 'test', whitebox: [], blackbox: [], password: 'test123')
-        user.save 
-
-        expect(user.whitebox.class).to eq(Array)
-        expect(user.blackbox.class).to eq(Array)
-        expect(user.vip).to be_falsey
-        expect(user.default_blackbox_message).to eq('User has denied the invitation')
-        expect(user.approved).to eq(1)
-        expect(user.appealed).to eq(1)
-        expect(user.access).to eq(2)
+        expect(@user.whitebox.class).to eq(Array)
+        expect(@user.blackbox.class).to eq(Array)
+        expect(@user.vip).to be_falsey
+        expect(@user.default_blackbox_message).to eq('User has denied the invitation')
+        expect(@user.approved).to eq(1)
+        expect(@user.appealed).to eq(1)
+        expect(@user.access).to eq(2)
       end 
     end 
   end   
