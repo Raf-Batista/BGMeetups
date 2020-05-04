d7c3425192454c153a8351d92c5802c13f99b945
Rafael <RafaelB2424@gmail.com>
2020-05-04 15:37:05 -0700

    Add post tests

--- bg-meetups-back-end/spec/models/post_spec.rb
+++ bg-meetups-back-end/spec/models/post_spec.rb
@@ -7,30 +7,15 @@ RSpec.describe Post, type: :model do
   end 
 
   before(:example) do 
-    user = User.create(name: 'test', email: 'test@email.com', password: 'test123')
-    user.groups.create(name: 'test', purpose: 'test', user: user).save
-    
-    @valid_post = user.posts.create(title: 'test title', content: 'test content', group: user.groups.last)
-    @invalid_post = Post.new(title: '', content: '', user_id: user.id, group_id: user.groups.last.id)
+    @user = User.create(name: 'test', email: 'test@email.com', password: 'test123')
+    @user.groups.create(name: 'test', purpose: 'test', user: @user).save
   end 
+ 
 
-  describe 'valid attributes' do 
-    it 'should have a title' do 
-    
-    end 
-
-    it 'should have content' do 
-
-    end 
-  end 
-
-  describe 'invalid attributes' do 
-    it 'can not be created without a title' do 
-
-    end 
-
-    it 'can not be created without content' do 
-
+  describe 'can not be created with invalid attributes' do 
+    it 'can not be created without a title or content' do 
+      expect(@user.posts.create(title: '', content: 'test').save).to be_falsey
+      expect(@user.posts.create(title: 'test', content: '').save).to be_falsey
     end 
   end 
 end
