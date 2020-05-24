8a905cf9eebbb9a48c5dbab20b2fe6fddded9982
= <RafaelB2424@gmail.com>
2020-05-23 20:02:32 -0700

    Add tests for Meetup

--- bg-meetups-back-end/spec/models/meetup_spec.rb
+++ bg-meetups-back-end/spec/models/meetup_spec.rb
@@ -7,12 +7,28 @@ RSpec.describe Meetup, type: :model do
 
   describe 'attributes' do 
     it 'should have a time with a datetime format' do 
-      group = Group.create(name: 'test', purpose: 'test')
-      binding.pry
+      user = User.create(name: 'test', email: 'test@email.com', password: 'test123')
+      group = Group.create(name: 'test', purpose: 'test', user_id: user.id)
+      group.meetups.create(meetup_time: '12:00')
+
+      expect(group.meetups.last.meetup_time).to be_truthy
+      expect(group.meetups.last.meetup_time.class).to eq(ActiveSupport::TimeWithZone)
     end 
 
-    it 'should have an array of those attended' 
+    it 'should have an array of those attended' do
+      user = User.create(name: 'test', email: 'test@email.com', password: 'test123')
+      group = Group.create(name: 'test', purpose: 'test', user_id: user.id)
+      group.meetups.create(meetup_time: '12:00')
 
-    it 'should have an array of those who did not attend'
+      expect(group.meetups.last.attended.class).to eq(Array)
+    end
+
+    it 'should have an array of those who did not attend' do
+      user = User.create(name: 'test', email: 'test@email.com', password: 'test123')
+      group = Group.create(name: 'test', purpose: 'test', user_id: user.id)
+      group.meetups.create(meetup_time: '12:00')
+
+      expect(group.meetups.last.no_show.class).to eq(Array)
+    end
   end 
 end
