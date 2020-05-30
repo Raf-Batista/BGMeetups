8c824e062c9bf7f1c02f8b09f8632795407294b0
Rafael Batista <RafaelB2424@gmail.com>
2020-05-29 21:16:20 -0400

    Add react router and home route

--- bg-meetups-front-end/src/App.js
+++ bg-meetups-front-end/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+       
+      </div>
+    </Router>
   );
 }
 
--- bg-meetups-front-end/src/components/Navbar.js
+++ bg-meetups-front-end/src/components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">BoardGameMeetups</a>
+            <a class="navbar-brand" href="/">BoardGameMeetups</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
