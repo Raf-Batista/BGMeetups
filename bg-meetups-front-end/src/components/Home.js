4761c092f512fe18e46ec6b968344e87c082c9cd
Rafael Batista <RafaelB2424@gmail.com>
2020-05-29 20:00:54 -0400

    Create Signup component

--- bg-meetups-front-end/src/components/Home.js
+++ bg-meetups-front-end/src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Signup from './Signup';
 
 const Home = () => {
     return (
         <div>
             Home Page
+            <Signup />
         </div>
     )
 }
