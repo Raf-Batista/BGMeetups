238e12a2baf794ac85386b4ac9c9ba115933f24a
Rafael Batista <RafaelB2424@gmail.com>
2020-05-29 20:26:33 -0400

    Style the Signup component form

--- bg-meetups-front-end/src/components/Signup.js
+++ bg-meetups-front-end/src/components/Signup.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Signup = () => {
     return (
-        <div>
-            Signup Page
+        <div className='container w-25 mt-3'>
+            <h1>Signup</h1>
+            <form>
+                <div className='form-group'>
+                    <input className='form-control mt-2' type='text' placeholder='username'/>
+                    <input className='form-control mt-2' type='password' placeholder='password'/>
+                    <button className='btn btn-primary mt-2'>Create</button>
+                </div>
+            </form>
         </div>
     )
 }
