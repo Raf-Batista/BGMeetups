c1ea6135decc018f64d6371537ea041cf3e8620a
Rafael Batista <RafaelB2424@gmail.com>
2020-06-01 17:56:20 -0400

    Add a heading and a paragraph to home page

--- bg-meetups-front-end/src/components/Home.js
+++ bg-meetups-front-end/src/components/Home.js
@@ -4,6 +4,10 @@ import Signup from "./Signup";
 const Home = () => {
   return (
     <div>
+      <div className='text-center my-5'>
+        <h1>Welcome to Board Game Meetups</h1>
+        <p className='h3'>Here you can create groups and invite people. Create meetups and play board games virtually through a web cam</p>
+      </div>
       <Signup />
     </div>
   );
