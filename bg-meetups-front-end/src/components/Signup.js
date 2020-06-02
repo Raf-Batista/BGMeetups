fb0ef368f18f55f0ebc96f44bcacefa2482c6b6e
= <RafaelB2424@gmail.com>
2020-06-01 19:05:17 -0700

    Add margin to span in third party login buttons

--- bg-meetups-front-end/src/components/Signup.js
+++ bg-meetups-front-end/src/components/Signup.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Signup = () => {
   const [user, setUser] = useState({
-      username: '',
-      email: '', 
-      password: '',
+    username: "",
+    email: "",
+    password: "",
   });
 
   const handleChange = (e) => {
     setUser({
-        ...user,
-        [e.target.name]: e.target.value
+      ...user,
+      [e.target.name]: e.target.value,
     });
-  }
-
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +23,7 @@ const Signup = () => {
   return (
     <div className='container text-center mt-4 signin'>
       <h2>Sign in Below</h2>
-      
+
       <div className='mt-4'>
         <button className=' btn btn-primary text-center'>
           <FontAwesomeIcon
@@ -32,17 +31,17 @@ const Signup = () => {
             size='lg'
             className='mr-2'
           />
-          <span>Facebook</span>
+          <span className='mr-2'>Facebook</span>
         </button>
       </div>
-      <div className='mb-2'>
+      <div className='my-2'>
         <button className='btn btn-success text-center'>
           <FontAwesomeIcon
             icon={["fab", "facebook"]}
             size='lg'
             className='mr-2'
           />
-          <span className=''>Twitter </span>
+          <span className='mr-4'>Twitter</span>
         </button>
       </div>
       <div className='mb-2'>
@@ -52,17 +51,38 @@ const Signup = () => {
             size='lg'
             className='mr-2'
           />
-          <span>Google</span>
+          <span className='mr-4'>Google</span>
         </button>
       </div>
-    <p className='mt-4'>Or create an account using your email</p>
+      <p className='mt-4'>Or create an account using your email</p>
       <div className='d-flex justify-content-center mt-2'>
-        <form onSubmit={handleSubmit} >
-            <div className='form-group'>
-                <input className='form-control' name='username' type='text' placeholder='username' onChange={handleChange} value={user.username}/>
-                <input className='form-control' name='email' type='email' placeholder='email' onChange={handleChange} value={user.email}/>
-                <input className='form-control' name='password' type='password' placeholder='password' onChange={handleChange} value={user.password}/>
-            </div>
+        <form onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <input
+              className='form-control'
+              name='username'
+              type='text'
+              placeholder='username'
+              onChange={handleChange}
+              value={user.username}
+            />
+            <input
+              className='form-control'
+              name='email'
+              type='email'
+              placeholder='email'
+              onChange={handleChange}
+              value={user.email}
+            />
+            <input
+              className='form-control'
+              name='password'
+              type='password'
+              placeholder='password'
+              onChange={handleChange}
+              value={user.password}
+            />
+          </div>
         </form>
       </div>
     </div>
