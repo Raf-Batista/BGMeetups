215d3f3d3edea980b286f4f24a13a7c073b3c5d3
Rafael Batista <RafaelB2424@gmail.com>
2020-06-08 19:39:59 -0400

    Create Navbar test

---
+++ bg-meetups-front-end/src/__tests__/components/Navbar.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Navbar from '../../components/Navbar';
+
+test('has the correct links in navbar',() => {
+    const wrapper = shallow(<Navbar />)
+    
+});
