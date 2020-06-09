b9b0083c23030c59161fc6b29a28a2f47a5fd723
Raf-Batista <RafaelB2424@gmail.com>
2020-06-08 18:10:51 -0700

    Refactor Navbar to use factories

--- bg-meetups-front-end/src/__tests__/components/Navbar.test.js
+++ bg-meetups-front-end/src/__tests__/components/Navbar.test.js
@@ -2,16 +2,39 @@ import React from "react";
 import { shallow } from "enzyme";
 import Navbar from "../../components/Navbar";
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @function setup
+ * @param {object} props -Component props specific to this setup
+ * @param {any} state - Initial state for setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}, state = null) => {
+  return shallow(<Navbar {...props} />);
+};
+
+/**
+ * Return ShallowWrapper containing node(s) with the given data-test value
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within
+ * @param {String} val - value of data-test attribute for search
+ * @returns {ShallowWrapper}
+ */
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
 test("renders the navbar component", () => {
-  const wrapper = shallow(<Navbar />);
-  const navbarComponent = wrapper.find("[data-test='component-navbar']");
+  const wrapper = setup();
+  const navbarComponent = findByTestAttr(wrapper, "component-navbar");
 
   expect(navbarComponent.length).toBe(1);
 });
 
 test("renders the links", () => {
-  const wrapper = shallow(<Navbar />);
-  const link = wrapper.find("[data-test='link-navbar']");
+  const wrapper = setup();
+  const link = findByTestAttr(wrapper, "link-navbar");
 
   expect(link.length).toBe(4);
 });
