e5cd3997def3f10e0495a8615f9f07d9dab6b003
Rafael Batista <RafaelB2424@gmail.com>
2020-06-10 16:45:37 -0400

    Create spec to check if the correct JSON response is returned when creating a user

--- bg-meetups-back-end/spec/requests/users_request_spec.rb
+++ bg-meetups-back-end/spec/requests/users_request_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
+    describe 'correct attributes' do
+        it 'can be created' do 
+            headers = { "ACCEPT" => "application/json" }
+            post '/users', params: {user: {username: 'test@email.com', username: 'test', password: 'test123'}}
+            json_response = JSON.parse(response.body)
+
+            expect(response.content_type).to eq("application/json")
+            expect(response).to have_http_status(:created)
+            expect(json_response.keys).to match('user')
+        end 
+    end
 
 end
