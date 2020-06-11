73c4434b775804f8ae4c0d38ee41f9d564cad9b9
Rafael Batista <RafaelB2424@gmail.com>
2020-06-11 15:48:10 -0400

    Add spec to check if a user was saved to the database

--- bg-meetups-back-end/spec/requests/users_request_spec.rb
+++ bg-meetups-back-end/spec/requests/users_request_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-    describe 'correct attributes' do
-        it 'can be created' do 
+    describe 'success' do
+        it 'returns the correct JSON response' do 
             headers = { "ACCEPT" => "application/json; charset=utf-8" }
             post '/users', params: {user: {email: 'test@email.com', username: 'test', password: 'test123'}}
             json_response = JSON.parse(response.body)
@@ -10,6 +10,12 @@ RSpec.describe "Users", type: :request do
             expect(response.content_type).to eq("application/json; charset=utf-8")
             expect(response).to have_http_status(:created)
             expect(json_response.keys).to match(['user'])
+        end 
+
+        it 'is saved to the database' do 
+            expect(User.all.size).to eq(0)
+            post '/users', params: {user: {email: 'test@email.com', username: 'test', password: 'test123'}}
+            expect(User.all.size).to eq(1)
         end 
     end
 
