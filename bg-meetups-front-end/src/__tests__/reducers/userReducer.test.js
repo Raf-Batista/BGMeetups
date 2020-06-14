0e5f5bbfe6d52624a5e1708247e3e09ca05bbae2
Rafael Batista <RafaelB2424@gmail.com>
2020-06-14 19:20:36 -0400

    Add lines to make tests more readable

--- bg-meetups-front-end/src/__tests__/reducers/userReducer.test.js
+++ bg-meetups-front-end/src/__tests__/reducers/userReducer.test.js
@@ -2,16 +2,19 @@ import userReducer from "../../reducers/userReducer";
 import * as types from "../../constants/user";
 
 describe("user reducer", () => {
+  
   it("should return the initial state", () => {
     expect(userReducer(undefined, {})).toEqual({});
   });
+
   it("should add a user to the state when fetch login request is successful", () => {
     const user = { username: "test", email: "test@email.com" };
     const expectedResult = { user:  user, isFetching: false};
     expect(userReducer({}, { type: types.FETCH_LOGIN_SUCCESS, payload: user })).toEqual(expectedResult);
   });
-  // it("should remove a user from the state", () => {
-  //   const user = { username: "test", email: "test@email.com" };
-  //   expect(userReducer({ user }, { type: types.LOGOUT })).toEqual({});
-  // });
+
+  it("should remove a user from the state", () => {
+    const user = { username: "test", email: "test@email.com" };
+    expect(userReducer({ user }, { type: types.LOGOUT })).toEqual({});
+  });
 });
