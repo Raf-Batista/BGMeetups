14b5ee8c9c8f726c70f1111a036121193c2caf18
Raf-Batista <RafaelB2424@gmail.com>
2020-06-14 06:37:38 -0700

    Add test to check if a user is added to the users state

--- bg-meetups-front-end/src/__tests__/reducers/usersReducer.test.js
+++ bg-meetups-front-end/src/__tests__/reducers/usersReducer.test.js
@@ -5,4 +5,11 @@ describe("users reducer", () => {
   it("should return initial state", () => {
     expect(usersReducer(undefined, {})).toEqual([]);
   });
+  it("should add a user to the state", () => {
+    const users = [{ username: "1" }, { username: "2" }];
+    const user = { username: "3" };
+    expect(
+      usersReducer(users, { type: types.ADD_USER, payload: user })
+    ).toEqual(users.concat(user));
+  });
 });
