7c38ade7380d141479f11bdc110bb1e2450ca8a8
Raf-Batista <RafaelB2424@gmail.com>
2020-06-14 07:21:38 -0700

    Add logout action creator

--- bg-meetups-front-end/src/actions/user.js
+++ bg-meetups-front-end/src/actions/user.js
@@ -1,5 +1,9 @@
 import * as types from "../constants/user";
 
+export const logout = () => ({
+  type: types.REMOVE_USER,
+});
+
 export const fetchUserRequest = (user) => ({
   type: types.FETCH_USER_REQUEST,
   payload: user,
