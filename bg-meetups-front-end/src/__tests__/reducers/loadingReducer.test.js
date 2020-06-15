61c124ca883862a996ddb6f15d5d4f47df23b4c5
Rafael Batista <RafaelB2424@gmail.com>
2020-06-15 17:30:49 -0400

    Add test to check if false is returned when not making a request

--- bg-meetups-front-end/src/__tests__/reducers/loadingReducer.test.js
+++ bg-meetups-front-end/src/__tests__/reducers/loadingReducer.test.js
@@ -3,7 +3,12 @@ import loadingReducer from '../../reducers/loadingReducer';
 describe('loading reducer', () => {
     it('returns true when action includes REQUEST', () => {
         const expectedResult = {'FETCH_TEST': true}
-        
         expect(loadingReducer({}, {type: 'FETCH_TEST_REQUEST'})).toEqual(expectedResult)
+    });
+
+    it('returns false when action does not include REQUEST', () => {
+        const expectedResult = {'FETCH_TEST': false}
+        expect(loadingReducer({}, {type: 'FETCH_TEST_SUCCESS'})).toEqual(expectedResult)
+        expect(loadingReducer({}, {type: 'FETCH_TEST_FAILURE'})).toEqual(expectedResult)
     });
 });
