37f04c806a0f4022b99093ac76ab8e3296a49577
Rafael Batista <RafaelB2424@gmail.com>
2020-06-15 17:26:23 -0400

    Change action name to FETCH_TEST_REQUEST

--- bg-meetups-front-end/src/__tests__/reducers/loadingReducer.test.js
+++ bg-meetups-front-end/src/__tests__/reducers/loadingReducer.test.js
@@ -2,8 +2,8 @@ import loadingReducer from '../../reducers/loadingReducer';
 
 describe('loading reducer', () => {
     it('returns true when action includes REQUEST', () => {
-        const expectedResult = {'FETCH_LOGIN': true}
+        const expectedResult = {'FETCH_TEST': true}
         
-        expect(loadingReducer({}, {type: 'FETCH_LOGIN_REQUEST'})).toEqual(expectedResult)
+        expect(loadingReducer({}, {type: 'FETCH_TEST_REQUEST'})).toEqual(expectedResult)
     });
 });
