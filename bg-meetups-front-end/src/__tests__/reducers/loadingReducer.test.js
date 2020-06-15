88d42e05b53aa72bad6f7403db7eccf9b832fbb9
Rafael Batista <RafaelB2424@gmail.com>
2020-06-15 17:24:03 -0400

    Add expected result to loadingReducer test which checks if a REQUEST action returns true

--- bg-meetups-front-end/src/__tests__/reducers/loadingReducer.test.js
+++ bg-meetups-front-end/src/__tests__/reducers/loadingReducer.test.js
@@ -2,8 +2,8 @@ import loadingReducer from '../../reducers/loadingReducer';
 
 describe('loading reducer', () => {
     it('returns true when action includes REQUEST', () => {
-        const test = loadingReducer({}, {type: 'FETCH_LOGIN_REQUEST'}); 
-
-        console.log(test)
+        const expectedResult = {'FETCH_LOGIN': true}
+        
+        expect(loadingReducer({}, {type: 'FETCH_LOGIN_REQUEST'})).toEqual(expectedResult)
     });
 });
