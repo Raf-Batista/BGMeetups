66115118266c9df2519f75ca98b9688962ef3efe
Rafael Batista <RafaelB2424@gmail.com>
2020-06-24 15:20:50 -0400

    Change json response and create user for spec, add session#create and appropriate route

--- bg-meetups-back-end/app/controllers/sessions_controller.rb
+++ bg-meetups-back-end/app/controllers/sessions_controller.rb
@@ -1,3 +1,9 @@
 class SessionsController < ApplicationController
-    
+    def create 
+        # Refactor 
+        # will return false if user does not exist or user can not be authenticated
+        user = User.find_by(email: params[:user][:email]) && User.find_by(email: params[:user][:email]).authenticate(params[:user][:password])
+        render json: {user: user}, status: :created and return if user
+        render json: {error: 'An error occured'}, status: :not_acceptable
+    end 
 end
--- bg-meetups-back-end/spec/requests/sessions_request_spec.rb
+++ bg-meetups-back-end/spec/requests/sessions_request_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe "Sessions", type: :request do
     describe 'successful login' do 
         it 'returns the correct JSON response' do 
+            User.create(username: 'test', email: 'test@email.com', password: 'test123')
             headers = { "ACCEPT" => "application/json; charset=utf-8" }
-            post '/users', params: {user: {email: 'test@email.com', password: 'test123'}}
+            post '/sessions', params: {user: {email: 'test@email.com', password: 'test123'}}
             json_response = JSON.parse(response.body)
 
             expect(response.content_type).to eq("application/json; charset=utf-8")
             expect(response).to have_http_status(:created)
             expect(json_response.keys).to match(['user'])
-            expect(json_response[:username]).to be_truthy
+            expect(json_response['user']['username']).to be_truthy
         end 
         
     end 
