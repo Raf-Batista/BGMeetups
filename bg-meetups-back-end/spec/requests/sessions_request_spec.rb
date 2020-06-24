e29c5bb8fc4183ce1b02ebb76e347171e5f21e72
Rafael Batista <RafaelB2424@gmail.com>
2020-06-24 00:07:24 -0400

    Create sessions controller and add spec to check if a username is returned when logging in

---
+++ bg-meetups-back-end/spec/requests/sessions_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Sessions", type: :request do
+    describe 'successful login' do 
+        it 'returns the correct JSON response' do 
+            headers = { "ACCEPT" => "application/json; charset=utf-8" }
+            post '/users', params: {user: {email: 'test@email.com', password: 'test123'}}
+            json_response = JSON.parse(response.body)
+
+            expect(response.content_type).to eq("application/json; charset=utf-8")
+            expect(response).to have_http_status(:created)
+            expect(json_response.keys).to match(['user'])
+            expect(json_response[:username]).to be_truthy
+        end 
+        
+    end 
+end
