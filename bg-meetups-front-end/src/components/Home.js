89a46944dce187352700931ff2a8509aaa7366ab
Raf-Batista <RafaelB2424@gmail.com>
2020-06-24 18:00:28 -0700

    Add responsiveness on small screens to carousel

--- bg-meetups-front-end/src/components/Home.js
+++ bg-meetups-front-end/src/components/Home.js
@@ -1,20 +1,26 @@
 import React from "react";
 import Signup from "./Signup";
-import bg1 from "../assets/images/bg1.jpg"
-import bg2 from "../assets/images/bg2.jpg"
-import bg3 from "../assets/images/bg3.jpg"
-import bg4 from "../assets/images/bg4.jpg"
-import bg5 from "../assets/images/bg5.jpg"
-
+import bg1 from "../assets/images/bg1.jpg";
+import bg2 from "../assets/images/bg2.jpg";
+import bg3 from "../assets/images/bg3.jpg";
+import bg4 from "../assets/images/bg4.jpg";
+import bg5 from "../assets/images/bg5.jpg";
 
 const Home = () => {
-  console.log(bg1)
+  console.log(bg1);
   return (
-    <div className='container text-center mt-5 home-img'>
-      
-          <div id="carouselExampleIndicators" className="carousel slide mt-5" data-ride="carousel">
+    <div className="container text-center mt-5 home-img">
+      <div
+        id="carouselExampleIndicators"
+        className="carousel slide mt-5"
+        data-ride="carousel"
+      >
         <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            class="active"
+          ></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
@@ -37,21 +43,25 @@ const Home = () => {
             <img src={bg5} class="d-block w-100" alt="..." />
           </div>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <a
+          class="carousel-control-prev"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="prev"
+        >
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <a
+          class="carousel-control-next"
+          href="#carouselExampleIndicators"
+          role="button"
+          data-slide="next"
+        >
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
       </div>
-        
-         
-        
-
-    
-     
     </div>
   );
 };
