a970d14281530bdf25f57e58cc77f07223cd375f
Raf-Batista <RafaelB2424@gmail.com>
2020-06-26 12:42:45 -0700

    Add conditional to check if a user is logged in and render the appropriate component

--- bg-meetups-front-end/src/components/Account.js
+++ bg-meetups-front-end/src/components/Account.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Login from "./Login";
+import { useSelector, useDispatch } from "react-redux";
 
 const Account = () => {
+  const loggedIn = useSelector((state) => state.loading);
+
   return (
     <div>
-      Account
-      <Login />
+      {/* If not logged in, "loading" in the store will be an empty object */}
+      {JSON.stringify(loggedIn) === "{}" ? <Login /> : <h1>Account</h1>}
     </div>
   );
 };
