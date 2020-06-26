0d0583599bf1acba0b9381ad6f7cec0295a946de
Raf-Batista <RafaelB2424@gmail.com>
2020-06-26 12:51:54 -0700

    Redirect to /login if not logged in

--- bg-meetups-front-end/src/components/Account.js
+++ bg-meetups-front-end/src/components/Account.js
@@ -2,13 +2,17 @@ import React from "react";
 import Login from "./Login";
 import { useSelector, useDispatch } from "react-redux";
 
-const Account = () => {
+const Account = (props) => {
   const loggedIn = useSelector((state) => state.loading);
 
   return (
     <div>
       {/* If not logged in, "loading" in the store will be an empty object */}
-      {JSON.stringify(loggedIn) === "{}" ? <Login /> : <h1>Account</h1>}
+      {JSON.stringify(loggedIn) === "{}" ? (
+        props.history.push("/login")
+      ) : (
+        <h1>Account</h1>
+      )}
     </div>
   );
 };
