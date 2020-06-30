f0a7bf083ad9213865f0c5f914887329318f771a
Rafael Batista <RafaelB2424@gmail.com>
2020-06-29 22:11:41 -0400

    Revert "Revert "Add reducer test to check if a user is added to the store after signing up""
    This reverts commit 321281b2e5f6d93545b366fdede705492babe6c9.

--- bg-meetups-front-end/src/__tests__/reducers/userReducer.test.js
+++ bg-meetups-front-end/src/__tests__/reducers/userReducer.test.js
@@ -6,6 +6,14 @@ describe("user reducer", () => {
     expect(userReducer(undefined, {})).toEqual({});
   });
 
+  it("should add a user to the state when fetch signup request is successful", () => {
+    const user = { username: "test", email: "test@email.com" };
+    const expectedResult = user;
+    expect(
+      userReducer({}, { type: types.FETCH_SIGNUP_SUCCESS, payload: user })
+    ).toEqual(expectedResult);
+  });
+
   it("should add a user to the state when fetch login request is successful", () => {
     const user = { username: "test", email: "test@email.com" };
     const expectedResult = user;
