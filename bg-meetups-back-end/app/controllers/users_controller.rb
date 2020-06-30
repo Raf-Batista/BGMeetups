cc285430155f29b5bacb0a5b32f8839276f122b2
Rafael Batista <RafaelB2424@gmail.com>
2020-06-29 21:10:31 -0400

    Remove comments in Home and clean up users controller

--- bg-meetups-back-end/app/controllers/users_controller.rb
+++ bg-meetups-back-end/app/controllers/users_controller.rb
@@ -1,16 +1,17 @@
 class UsersController < ApplicationController
     def create # Refactor 
         user = User.new(user_params)
-        # attach an identicon as a user's avatar when they are created if a user is valid
+        # attach an identicon as a user's avatar if they are created
         user.avatar.attach(data: Identicon.data_url_for(user.username)) if user.valid?
         render json: {user: user}, status: :created and return if user.save
         render json: {errors: user.errors.full_messages}, status: :not_acceptable and return if !user.save
+        
         render json: {errors: "An error occured"}, status: :not_acceptable
     end 
 
     private 
 
     def user_params
-        params.require(:user).permit(:email, :username, :password, :avatar)
+        params.require(:user).permit(:email, :username, :password)
     end 
 end
