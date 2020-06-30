4c88d00ad425796d5d40ba9a283e0b7d9abb15f9
Rafael Batista <RafaelB2424@gmail.com>
2020-06-30 16:27:40 -0400

    Create fetch signup async action

--- bg-meetups-front-end/src/async_actions/fetchSignup.js
+++ bg-meetups-front-end/src/async_actions/fetchSignup.js
@@ -1,5 +1,30 @@
-const fetchSignup = () => {
+import * as actions from "../actions/user";
+const URL = `${process.env.REACT_APP_URL}/users`;
 
+const fetchSignup = (params) => {
+    return async (dispatch) => {
+        
+        dispatch(actions.fetchSignupSuccess());
+        
+        try {
+            const response = await fetch(URL, {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: "POST",
+                body: JSON.stringify(params)
+            });
+
+            const result = await response.json();
+
+            if(!result.errors) return dispatch(actions.fetchSignupSuccess(result));
+
+            dispatch(actions.fetchSignupFailure(result.errors))
+        
+        } catch (error) {
+            console.log(error)
+        }
+    }
 };
 
 export default fetchSignup;
