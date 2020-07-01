671c1c7f189ea2338d7e529cb936dca3e3a31dc0
Rafael Batista <RafaelB2424@gmail.com>
2020-07-01 18:33:22 -0400

    Add presence true to email and uniqueness to username

--- bg-meetups-back-end/app/models/user.rb
+++ bg-meetups-back-end/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :username, presence: true 
+    validates :username, uniqueness: true, presence: true 
   # validates :interest, presence: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true 
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
     has_many :members 
     has_many :groups, through: :members
     has_many :posts
