a39fb513ef12ff93e7ec79a256a83e354e351143
Rafael Batista <RafaelB2424@gmail.com>
2020-06-30 20:25:22 -0400

    Render username and email only

--- bg-meetups-back-end/app/controllers/sessions_controller.rb
+++ bg-meetups-back-end/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
         # Refactor 
         # will return false if user does not exist or user can not be authenticated
         user = User.find_by(email: params[:email]) && User.find_by(email: params[:email]).authenticate(params[:password])
-        render json: {user: user}, status: :created and return if user
+        render json: {user: {email: user.email, username: user.username}}, status: :created, adapter: :json and return if user
         render json: {error: 'An error occured'}, status: :not_acceptable
     end 
 end
