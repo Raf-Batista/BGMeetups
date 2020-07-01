d5ac3314361ac2cc5e0528bf00379487e0f020e0
Rafael Batista <RafaelB2424@gmail.com>
2020-06-30 20:25:51 -0400

    Render username and email only

--- bg-meetups-back-end/app/controllers/users_controller.rb
+++ bg-meetups-back-end/app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+
+    def index 
+        render json: User.all
+    end 
+
     def create # Refactor 
         user = User.new(user_params)
         # attach an identicon as a user's avatar if they are created
-        binding.pry
         user.avatar.attach(data: Identicon.data_url_for(user.username)) if user.valid?
-        render json: {user: user}, status: :created and return if user.save
+        render json: {user: {email: user.email, username: user.username}}, status: :created and return if user.save
         render json: {errors: user.errors.full_messages}, status: :not_acceptable and return if !user.save
         
         render json: {errors: "An error occured"}, status: :not_acceptable
