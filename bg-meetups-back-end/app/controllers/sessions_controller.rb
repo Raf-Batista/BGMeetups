cdbb50dae0d86af626046076d8a1babe92ea29d7
Rafael Batista <RafaelB2424@gmail.com>
2020-07-09 19:49:12 -0400

    Add destroy and create method, authenticates cookie before destroy action

--- bg-meetups-back-end/app/controllers/sessions_controller.rb
+++ bg-meetups-back-end/app/controllers/sessions_controller.rb
@@ -1,9 +1,31 @@
 class SessionsController < ApplicationController
-    def create 
-        # Refactor 
-        # will return false if user does not exist or user can not be authenticated
-        user = User.find_by(email: params[:email]) && User.find_by(email: params[:email]).authenticate(params[:password])
-        render json: {email: user.email, username: user.username, avatar: url_for(user.avatar)}, status: :created, adapter: :json and return if user
-        render json: {error: 'An error occured'}, status: :not_acceptable
-    end 
+    before_action only: [:destroy] do 
+        authenticate_cookie
+      end
+    
+      def destroy
+        if current_user 
+          cookies.delete(:jwt) 
+          render json: {status: 'OK', code: 200} and return
+        end
+          
+        render json: {status: 'session not found', code: 404}
+      end
+    
+      def create
+        login_hash = User.handle_login(params["email"], params["password"])
+          if login_hash
+            cookies.signed[:jwt] = {value: login_hash[:token], httponly: true}
+            render json: { 
+              id: login_hash[:id],
+              username: login_hash[:username],
+              email: login_hash[:email],
+              avatar: url_for(login_hash[:avatar])
+            }, status: :created, adapter: :json and return
+          else
+            render json: {status: 'incorrect email or password', code: 422} and return 
+          end
+        
+        render json: {status: 'specify email address and password', code: 422}
+      end 
 end
