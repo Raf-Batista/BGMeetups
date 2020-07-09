5c4615c86e4ef1c9e2ed11002821a4f4589c4fdd
Rafael Batista <RafaelB2424@gmail.com>
2020-07-09 19:50:32 -0400

    Add handle login class method

--- bg-meetups-back-end/app/models/user.rb
+++ bg-meetups-back-end/app/models/user.rb
@@ -9,4 +9,19 @@ class User < ApplicationRecord
     has_many :posts
     has_many :messages
     has_one_base64_attached :avatar
+
+    def self.handle_login(email, password)
+      user = User.find_by(email: email.downcase)
+      if user && user.authenticate(password)
+        user_info = {}
+        user_info[:token] = CoreModules::JsonWebToken.encode({user_id: user.id}, 4.hours.from_now)
+        user_info[:id] = user.id
+        user_info[:username] = user.username
+        user_info[:email] = user.email
+        user_info[:avatar] = user.avatar
+        return user_info
+      end
+        
+     return false
+    end
 end
