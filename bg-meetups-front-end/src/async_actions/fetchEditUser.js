797eea519f2ff524fc6faebed813ef903c4679d2
Rafael Batista <RafaelB2424@gmail.com>
2020-07-19 14:59:14 -0400

    Create fetch request to edit a user

---
+++ bg-meetups-front-end/src/async_actions/fetchEditUser.js
@@ -0,0 +1,45 @@
+import * as actions from "../actions/user";
+import { toast } from 'react-toastify';
+
+const URL = `${process.env.REACT_APP_URL}/users`;
+
+const displayErrors = (errorsArray) => {
+    errorsArray.forEach(error => {
+        toast.error(error, {
+            position: toast.POSITION.TOP_CENTER
+        });
+    });
+};
+
+const fetchEditUser = (params) => {
+    return async (dispatch) => {
+
+        dispatch(actions.fetchEditRequest());
+
+        try {
+            const response = await fetch(`${URL}/${params.id}`, {
+                credentials: "include",
+                headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                },
+                method: "PATCH",
+                body: JSON.stringify(params),
+            });
+
+            const result = await response.json();
+
+            if(!result.error) {
+                toast.success("Edit Successful", {position: toast.POSITION.TOP_CENTER});
+                dispatch(actions.fetchEditSuccess(result));
+            }
+
+            displayErrors(result.error);
+            dispatch(actions.fetchEditFailure(result.error));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export default fetchEditUser;
