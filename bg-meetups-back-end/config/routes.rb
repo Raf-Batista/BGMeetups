69863ae1a50b31e69ef42474b41df60426c213a7
Rafael Batista <RafaelB2424@gmail.com>
2020-07-22 19:42:58 -0400

    Add default url

--- bg-meetups-back-end/config/routes.rb
+++ bg-meetups-back-end/config/routes.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options[:host] = "localhost"
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users #, only: [:index, :create, :edit, :delete]
