05d395c0e68aa64b572cc35e0724f191fd8727b4
Rafael Batista <RafaelB2424@gmail.com>
2020-07-22 19:42:24 -0400

    Return rails blob path for avatar

--- bg-meetups-back-end/app/serializers/user_serializer.rb
+++ bg-meetups-back-end/app/serializers/user_serializer.rb
@@ -1,13 +1,18 @@
 class UserSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :username, :email, :avatar
+  attributes :id, :username, :email, :avatar, :messages
 
-  # def avatar
-  #   if object.featured_image.attached?
-  #     {
-  #       url: rails_blob_url(object.avatar)
-  #     }
-  #   end
-  # end
+  def avatar
+    if object.avatar.attached?
+      # URI.join(ActionController::Base.asset_host, rails_blob_path(object.avatar))
+      # url_for(object.avatar)
+      rails_blob_path(object.avatar)
+      # attribute :verification_url do
+      #   if object.verification_file.attachment
+      #     URI.join(ActionController::Base.asset_host, rails_blob_path(object.verification_file))
+      #   end
+      # end
+    end
+  end
 end
