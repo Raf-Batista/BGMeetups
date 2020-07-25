5f6f07ed3e2ea2fde220ef44d51351aedf823a9b
Rafael Batista <RafaelB2424@gmail.com>
2020-07-25 11:17:39 -0400

    Render boardgame information from logged in user as test data

--- bg-meetups-front-end/src/components/Market.js
+++ bg-meetups-front-end/src/components/Market.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Market = () => {
-  return <div>MARKET</div>;
-};
+const Market = (props) => {
+  const {boardgame} = props;
+  return( 
+    <div className='container text-center my-4'>
+      <div className=''>{boardgame.name}</div>
+      <div>{boardgame.description}</div>
+      <div>{boardgame.price}</div>
+      <button className='btn'>Contact Seller</button>
+    </div>
+  )};
 
 export default Market;
