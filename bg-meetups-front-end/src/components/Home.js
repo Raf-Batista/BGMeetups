df99bdf7ce8886119eb8b84a92887bf068c35251
Rafael Batista <RafaelB2424@gmail.com>
2020-07-27 19:22:43 -0400

    Change class to className

--- bg-meetups-front-end/src/components/Home.js
+++ bg-meetups-front-end/src/components/Home.js
@@ -13,51 +13,51 @@ const Home = () => {
         className="carousel slide mt-5"
         data-ride="carousel"
       >
-        <ol class="carousel-indicators">
+        <ol className="carousel-indicators">
           <li
             data-target="#carouselExampleIndicators"
             data-slide-to="0"
-            class="active"
+            className="active"
           ></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
           <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
         </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
             <img src={bg1} class="d-block w-100" alt="..." />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img src={bg2} class="d-block w-100" alt="..." />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img src={bg3} class="d-block w-100" alt="..." />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img src={bg4} class="d-block w-100" alt="..." />
           </div>
-          <div class="carousel-item">
+          <div className="carousel-item">
             <img src={bg5} class="d-block w-100" alt="..." />
           </div>
         </div>
         <a
-          class="carousel-control-prev"
+          className="carousel-control-prev"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="prev"
         >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
         </a>
         <a
-          class="carousel-control-next"
+          className="carousel-control-next"
           href="#carouselExampleIndicators"
           role="button"
           data-slide="next"
         >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
         </a>
       </div>
     </div>
