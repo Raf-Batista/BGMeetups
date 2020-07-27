1797497907859095f6b5a76bd5fa36da744531df
Rafael Batista <RafaelB2424@gmail.com>
2020-07-27 19:24:17 -0400

    Get state from store and add function to get all boardgames belonging to users

--- bg-meetups-front-end/src/containers/MarketContainer.js
+++ bg-meetups-front-end/src/containers/MarketContainer.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Market from '../components/Market';
 import { useSelector } from 'react-redux';
 import PacmanLoader from "react-spinners/PacmanLoader";
 
 const MarketContainer = () => {
-    const boardgames = useSelector(state => state.user.boardgames)
+    const users = useSelector(state => state.users);
+
+    const getBoardgames = (users) => {
+        const boardgames = [];
+        users.forEach((user) => {
+            if(!!user.boardgames.length) boardgames.push(user.boardgames);
+        })
+        return boardgames;
+    }
+    
+    const boardgames = getBoardgames(users).flat(1)
     return (
         <div>
-            {
+             {
                 boardgames ? 
                     boardgames.map((boardgame) => <Market boardgame={boardgame} key={boardgame.id}/>) :  
                     <div className="spinner">
                          <PacmanLoader color={"#7d3cff"} />
                     </div>
-            }
+            } 
         </div>
     )
 }
