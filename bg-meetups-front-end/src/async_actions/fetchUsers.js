c573f6478937b5dcd24c18560aaef67f62483993
Rafael Batista <RafaelB2424@gmail.com>
2020-07-27 19:21:51 -0400

    Create async action to fetch all users

---
+++ bg-meetups-front-end/src/async_actions/fetchUsers.js
@@ -0,0 +1,29 @@
+import * as actions from "../actions/users";
+
+const URL = `${process.env.REACT_APP_URL}/users`;
+
+const fetchUsers = () => {
+    return async (dispatch) => {
+        dispatch(actions.fetchUsersRequest())
+
+        try {
+            const response = await fetch(URL, {
+                credentials: "include",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                }
+            });
+
+            const result = await response.json();
+
+            if(!result.errors) return dispatch(actions.fetchUsersSuccess(result));
+
+            dispatch(actions.fetchUsersFailure(result.errors));
+        } catch (error) {
+            console.log(error)
+        }
+    }
+};
+
+export default fetchUsers;
