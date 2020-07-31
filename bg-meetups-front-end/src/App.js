f298e70e72e9437d18e8cb0f25f112aea3faee4f
Raf-Batista <RafaelB2424@gmail.com>
2020-07-31 12:19:25 -0700

    Import MarketContainer and MarketEditContainer, pass in empty array as second argument to useEffect to prevent infinite loop

--- bg-meetups-front-end/src/App.js
+++ bg-meetups-front-end/src/App.js
@@ -9,25 +9,25 @@ import Signup from "./components/Signup";
 import Account from "./components/Account";
 import Groups from "./components/Groups";
 import MarketContainer from "./containers/MarketContainer";
+import MarketEditContainer from "./containers/MarketEditContainer";
 import Login from "./components/Login";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useSelector, useDispatch } from 'react-redux';
-import fetchCurrentUser from './async_actions/fetchCurrentUser';
-import fetchUsers from './async_actions/fetchUsers';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useSelector, useDispatch } from "react-redux";
+import fetchCurrentUser from "./async_actions/fetchCurrentUser";
+import fetchUsers from "./async_actions/fetchUsers";
 
 library.add(fab);
 
 const App = () => {
-
-  const currentUser = useSelector(state => state.user);
-  const users = useSelector(state => state.users)
+  const currentUser = useSelector((state) => state.user);
+  const users = useSelector((state) => state.users);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(JSON.stringify(currentUser) === '{}') dispatch(fetchCurrentUser()); 
-    if(JSON.stringify(users) === '[]') dispatch(fetchUsers()); 
-  });
+    if (JSON.stringify(currentUser) === "{}") dispatch(fetchCurrentUser());
+    if (JSON.stringify(users) === "[]") dispatch(fetchUsers());
+  }, []); // empty array passed as second argument to prevent loop. https://stackoverflow.com/questions/53243203/react-hook-useeffect-runs-continuously-forever-infinite-loop
 
   return (
     <div>
@@ -39,12 +39,16 @@ const App = () => {
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/account" component={Account} />
-          <Route exact path="/groups" render={(routeProps) => <Groups {...routeProps} />} />
+          <Route
+            exact
+            path="/groups"
+            render={(routeProps) => <Groups {...routeProps} />}
+          />
           <Route exact path="/market" component={MarketContainer} />
+          <Route exact path="/my-market" component={MarketEditContainer} />
         </Switch>
-    </Router>
+      </Router>
     </div>
-
   );
 };
 
