92d5163d66a4d93d07082063a018bccbe0802186
Raf-Batista <RafaelB2424@gmail.com>
2020-07-31 12:22:15 -0700

    Add temporary text to div when there are no groups available

--- bg-meetups-front-end/src/components/Groups.js
+++ bg-meetups-front-end/src/components/Groups.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Groups = () => {
-  return <div>GROUPS</div>;
+  return <div className="text-center mt-5">It's Quiet... Too Quiet...</div>;
 };
 
 export default Groups;
