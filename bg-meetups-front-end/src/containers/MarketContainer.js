f01537194d3722e4bc719d6260ba90017f19eb4a
Raf-Batista <RafaelB2424@gmail.com>
2020-07-31 12:26:03 -0700

    Create MarketContainer to view boardgames a user has for sale as well as create a post to sell a boardgame

--- bg-meetups-front-end/src/containers/MarketContainer.js
+++ bg-meetups-front-end/src/containers/MarketContainer.js
@@ -1,31 +1,34 @@
-import React, { useEffect } from 'react';
-import Market from '../components/Market';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import Market from "../components/Market";
+import { useSelector } from "react-redux";
 import PacmanLoader from "react-spinners/PacmanLoader";
 
 const MarketContainer = () => {
-    const users = useSelector(state => state.users);
+  const users = useSelector((state) => state.users);
 
-    const getBoardgames = (users) => {
-        const boardgames = [];
-        users.forEach((user) => {
-            if(!!user.boardgames.length) boardgames.push(user.boardgames);
-        })
-        return boardgames;
+  const getBoardgames = (users) => {
+    const boardgames = [];
+    try {
+      users.forEach((user) => {
+        if (!!user.boardgames.length) boardgames.push(user.boardgames);
+      });
+      return boardgames.flat(1);
+    } catch (error) {
+      console.log(error);
     }
-    
-    const boardgames = getBoardgames(users).flat(1)
-    return (
-        <div>
-             {
-                boardgames ? 
-                    boardgames.map((boardgame) => <Market boardgame={boardgame} key={boardgame.id}/>) :  
-                    <div className="spinner">
-                         <PacmanLoader color={"#7d3cff"} />
-                    </div>
-            } 
-        </div>
-    )
-}
+  };
 
-export default MarketContainer
+  const boardgames = getBoardgames(users);
+
+  return (
+    <div>
+      {boardgames.length ? (
+        boardgames.map((boardgame) => <Market boardgame={boardgame} />)
+      ) : (
+        <div className=" mt-5 text-center">It's Quiet... Too Quiet...</div>
+      )}
+    </div>
+  );
+};
+
+export default MarketContainer;
