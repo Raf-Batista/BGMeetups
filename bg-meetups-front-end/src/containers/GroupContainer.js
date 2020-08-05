4a9000aa11ab276531fd4b448e7ad53f2ed5350b
Rafael Batista <RafaelB2424@gmail.com>
2020-08-05 11:03:10 -0400

    Add search bar

--- bg-meetups-front-end/src/containers/GroupContainer.js
+++ bg-meetups-front-end/src/containers/GroupContainer.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const GroupContainer = () => {
-  return <div></div>;
+  return <div>
+
+   <div className='d-inline'> 
+          <form className=' d-inline'>
+              <span className='input-group-addon'>
+                  <FontAwesomeIcon
+                  icon={faSearch}
+                  size="lg"
+                  className='search-icon'
+                  />
+              </span>
+              <input type='text' placeholder="Search for groups"/>
+          </form>
+      </div>
+  </div>;
 };
 
 export default GroupContainer;
