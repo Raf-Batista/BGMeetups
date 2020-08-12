44c7e71aee1a4e28ac254ae4986964aafb0b98ed
Rafael Batista <RafaelB2424@gmail.com>
2020-08-12 12:51:00 -0400

    Create migration to create table received_messages

---
+++ bg-meetups-back-end/db/migrate/20200812155609_create_received_messages.rb
@@ -0,0 +1,9 @@
+class CreateReceivedMessages < ActiveRecord::Migration[6.0]
+  def change
+    create_table :received_messages do |t|
+      t.string :from
+      t.string :subject
+      t.text :content
+    end
+  end
+end
