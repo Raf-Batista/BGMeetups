de44daf9f6249322e04c355ba1c0b59d19359e01
Rafael Batista <RafaelB2424@gmail.com>
2020-08-12 12:23:56 -0400

    Rename Message to SentMessage

--- bg-meetups-back-end/app/models/message.rb
+++
@@ -1,6 +0,0 @@
-class Message < ApplicationRecord
-    belongs_to :user
-    validates :recipient, presence: true
-    validates :subject, presence: true
-    validates :content, presence: true 
-end
