daf98b39e174b88699f561d8d79d297feec5ee4b
Rafael Batista <RafaelB2424@gmail.com>
2020-08-12 12:39:48 -0400

    Add action cable route

--- bg-meetups-back-end/config/routes.rb
+++ bg-meetups-back-end/config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.default_url_options[:host] = "localhost"
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users #, only: [:index, :create, :edit, :delete]
+  resources :users do #, only: [:index, :create, :edit, :delete]
+    resources :messages
+  end 
   post '/sessions', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/current_user', to: 'sessions#logged_in?'
+
+  mount ActionCable.server => '/cable'
 end
