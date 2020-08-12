03af870e6ee1b0ceb6e3c00bd3b40c4a8f2e484a
Rafael Batista <RafaelB2424@gmail.com>
2020-08-12 12:49:42 -0400

    Remove user_id field

--- bg-meetups-back-end/db/migrate/20200524032918_create_messages.rb
+++ bg-meetups-back-end/db/migrate/20200524032918_create_messages.rb
@@ -1,7 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-      t.integer :user_id
       t.string :recipient
       t.string :subject
       t.text :content
