43decb6e8e8214a85af5ece7e80cb52979b32e29
Rafael Batista <RafaelB2424@gmail.com>
2020-08-13 10:47:52 -0400

    add state and an onClick event listener, add columns with a div to switch between sent and receieved and placeholder data to style the messages

--- bg-meetups-front-end/src/components/Messages.js
+++ bg-meetups-front-end/src/components/Messages.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Messages = () => {
+    const [active, setActive] = useState(true);
+
+    const handleClick = () => {
+        setActive(!active);
+      };
+
     return (
-        <div>
+        <div className='container messages'>
+          <div className='row'>
             
+            <div className='col-6'>
+                    <div
+                      className={`nav-link pointer ${active ? "active" : ""}`}
+                      onClick={handleClick}
+                    >
+                    Recieved
+                    </div>
+                  <span
+                    className={`nav-link pointer ${!active ? "active" : ""}`}
+                    onClick={handleClick}
+                  >
+                    Sent
+                  </span>
+            </div>
+
+            <div className='col-6 mt-2'>
+              <div className='message-subjects'>THIS IS A TEST SUBJECT THIS IS A TEST SUBJECT</div>
+              <div className='message-subjects'>THIS IS A TEST</div>
+              <div className='message-subjects'>THIS IS A</div>
+              <div className='message-subjects'>THIS IS</div>
+              <div className='message-subjects'>THIS</div>
+              <div className='message-subjects'>THIS IS A TEST SUBJECT THIS IS A TEST SUBJECT</div>
+              <div className='message-subjects'>THIS IS A TEST</div>
+              <div className='message-subjects'>THIS IS A</div>
+              <div className='message-subjects'>THIS IS</div>
+              <div className='message-subjects'>THIS</div>
+            </div>
+          </div> 
         </div>
     )
 }
