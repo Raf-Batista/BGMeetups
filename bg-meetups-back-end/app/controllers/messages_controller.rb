757e84d2a14b51ca00f2d9befb984bd694362a28
Rafael Batista <RafaelB2424@gmail.com>
2020-08-13 10:43:19 -0400

    Change messages controller to create a sent and received message for the sender and receiver of a new message

--- bg-meetups-back-end/app/controllers/messages_controller.rb
+++ bg-meetups-back-end/app/controllers/messages_controller.rb
@@ -1,12 +1,14 @@
 class MessagesController < ApplicationController
     def create 
+      #  binding.pry
         user = current_user
-        message = Message.new(message_params) 
-        message.sender = user.username 
-        message.user = user
+        user.sent_messages.new(message_params)
+      #  message.sender = user.username 
         recipient = User.find_by(username: message_params[:recipient])
-        message.recipient = recipient.username
-        if message.save
+        recipient.received_messages.new(from: user.username, subject: message_params[:subject], content: message_params[:content])
+        if user.valid? && recipient.valid?
+            user.save 
+            recipient.save
             WebNotificationsChannel.broadcast_to(recipient, message: "#{user.username} sent you a message".to_json())
             render json: {message: "success"}
         end
