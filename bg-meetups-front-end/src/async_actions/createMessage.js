a2a87e39aaac0cc7d7658f003aa5661e4436d78c
Rafael Batista <RafaelB2424@gmail.com>
2020-08-18 18:34:27 -0400

    Add fetch request to create a message and dispatch a success or failure

--- bg-meetups-front-end/src/async_actions/createMessage.js
+++ bg-meetups-front-end/src/async_actions/createMessage.js
@@ -1,30 +1,36 @@
-import * as actions from "../actions/user";
+import * as actions from "../actions/message";
 import { toast } from "react-toastify";
 
 
-const createMessage = async (params, userId) => {
-  const URL = `${process.env.REACT_APP_URL}/users/${userId}/messages`;
+const createMessage = (params, userId) => {
+  return async (dispatch) => {
+    const URL = `${process.env.REACT_APP_URL}/users/${userId}/messages`;
+    dispatch(actions.sentMessageRequest());
+    try {
+      const response = await fetch(URL,{
+        credentials: "include",
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }, 
+        body: JSON.stringify(params)
+      });
+        
+      const result = await response.json();
+        
+      if(!result.error) {
+        toast.success("Message Sent", {position: toast.POSITION.TOP_CENTER});
+        return dispatch(actions.sentMessageSuccess(result));
+      }
 
-        try {
-          const response = await fetch(URL,{
-            credentials: "include",
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            }, 
-            body: JSON.stringify(params)
-          });
-            
-          const result = await response.json();
-          console.log(result)
-            
-          if(!result.error) toast.success("Message Sent", {position: toast.POSITION.TOP_CENTER});
-
-          if(result.error) toast.error(result.error, {position: toast.POSITION.TOP_CENTER});
-        } catch (error) {
-            console.log(error);
-        }
+      dispatch(actions.sentMessageFailure(result.error))
+      toast.error(result.error, {position: toast.POSITION.TOP_CENTER});
+    } catch (error) {
+        console.log(error);
+    }
+  }
+ 
     
 }
 
