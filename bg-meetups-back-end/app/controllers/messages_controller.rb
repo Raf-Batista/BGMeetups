031e9117478f13b49a479c8887aff1749b821ff5
Rafael Batista <RafaelB2424@gmail.com>
2020-08-18 18:16:46 -0400

    Refactor the create action

--- bg-meetups-back-end/app/controllers/messages_controller.rb
+++ bg-meetups-back-end/app/controllers/messages_controller.rb
@@ -1,16 +1,14 @@
 class MessagesController < ApplicationController
     def create 
-      #  binding.pry
         user = current_user
         user.sent_messages.new(message_params)
-      #  message.sender = user.username 
         recipient = User.find_by(username: message_params[:recipient])
         recipient.received_messages.new(from: user.username, subject: message_params[:subject], content: message_params[:content])
         if user.valid? && recipient.valid?
-            user.save 
-            recipient.save
-            WebNotificationsChannel.broadcast_to(recipient, message: "#{user.username} sent you a message".to_json())
-            render json: {message: "success"}
+            user.save && recipient.save
+            WebNotificationsChannel.broadcast_to(recipient, notification: "#{user.username} sent you a message".to_json(), receivedMessage: recipient.received_messages.last)
+            
+            render json: user.sent_messages.last
         end
     end 
 
