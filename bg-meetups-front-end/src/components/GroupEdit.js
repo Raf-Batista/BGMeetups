cdd97eac54a5d6c8231a0a1564b79b55579ed99c
Raf-Batista <RafaelB2424@gmail.com>
2020-08-19 12:55:05 -0700

    Create GroupEdit component with form and event listeners

---
+++ bg-meetups-front-end/src/components/GroupEdit.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+const GroupEdit = (props) => {
+  const [group, setGroup] = useState({
+    name: "",
+    purpose: "",
+  });
+
+  const handleChange = (e) => {
+    setGroup({
+      ...group,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setGroup({ name: "", purpose: "" });
+  };
+
+  const handleClick = (e) => {
+    setGroup({ name: "", purpose: "" });
+  };
+
+  return (
+    <form className="mt-4 mx-lg-auto ml-sm-4" onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="d-block my-2">Name</label>
+        <input
+          name="name"
+          type="text"
+          onChange={handleChange}
+          value={group.name}
+        />
+        <label className="d-block my-2">Purpose</label>
+        <input
+          name="purpose"
+          type="text"
+          onChange={handleChange}
+          value={group.purpose}
+        />
+
+        <div>
+          <button
+            className="d-inline mx-2 mt-2 border-0 bg-transparent"
+            type="submit"
+          >
+            Create
+          </button>
+          <span
+            className="d-inline border-0 bg-transparent pointer"
+            onClick={handleClick}
+          >
+            Cancel
+          </span>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default GroupEdit;
