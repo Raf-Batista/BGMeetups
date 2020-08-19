bdbfe94f5b172d366cffc8049fd87735b7cff8e7
Raf-Batista <RafaelB2424@gmail.com>
2020-08-19 14:34:39 -0700

    Create fetch request to create a new group

---
+++ bg-meetups-front-end/src/async_actions/createGroup.js
@@ -0,0 +1,34 @@
+import * as actions from "../actions/group";
+import { toast } from "react-toastify";
+
+const createGroup = (params, userId) => {
+  return async (dispatch) => {
+    try {
+      dispatch(actions.createGroupRequest());
+      const URL = `${process.env.REACT_APP_URL}/users/${userId}/groups`;
+      const response = await fetch(URL, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(params),
+      });
+
+      const result = await response.json();
+
+      if (!result.error) {
+        toast.success("Group Created", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+
+        return dispatch(actions.createGroupSuccess(result));
+      }
+
+      dispatch(actions.createGroupFailure(result.error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
