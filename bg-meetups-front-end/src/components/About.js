ddf4f23d3f882053861cc494e6659e27e3ef79d6
Rafael Batista <RafaelB2424@gmail.com>
2020-08-24 15:48:46 -0400

    Create Account component with details on upcoming chanages

--- bg-meetups-front-end/src/components/About.js
+++ bg-meetups-front-end/src/components/About.js
@@ -3,51 +3,40 @@ import React from 'react'
 const About = () => {
     return (
         <div className='container my-5 about'>
-            <span>Hi, I'm Rafael Batista and this is my personal dream project. BGMeetups will be an application where you can play boardgames virtually due to the pandemic without needing multiple copies of a game or any other hassle. This app will start out with basic features but will expand over time. It's just me working on this so this project has and will take time to get to where I envsion it to be. Below you'll find a list changes or features I am working on currently and then a list of the major features planned for the future.</span>
+            <span>Hi, I'm Rafael Batista and this is my personal dream project. BGMeetups will be an application where you can play boardgames virtually due to the pandemic without needing multiple copies of a game or any other hassle. This app will start out with basic features but will expand over time. The goal is to be able to activte Mecatol Rex virtually Below you will find a list of planned major features/updates.</span>
 
-            <h3 className='mt-5'>Current Changes / Features</h3>
+            <small className='d-block mt-3'>This site uses cookies for authentication and authorization. This is a work in progress so just use a fake email. There is no email system set up yet. No emails will ever be visible on the front end, the back end will handle everything email related.</small>
+
+            <h3 className='mt-5'>Planned Features</h3>
             <ul className='mt-3 list-group list-group-flush'>
+         
+                <li className='mt-2 list-group-item'>
+                    <h4>Create a Meetup</h4>
+                    <div>This feature will allow a user to create a meetup at a certain date or time. All user's who are a part of that group will receive a notification as well as an option to accept or deny the meetup.</div>
+                </li>
+                <li className='mt-2 list-group-item'>
+                    <h4>Zoom Integration</h4>
+                    <div>Zoom will be integrated for groups to use to play virtually.</div>
+                </li>
+                <li className='mt-2 list-group-item'>
+                    <h4>Email System</h4>
+                    <div>Once the application has enough features to function I will implement an email system with verification, etc.</div>
+                </li>
                 <li className='mt-3 list-group-item'>
-                    <h4>Change Design of Messages</h4>
+                    <h4>Third Pary Login</h4>
+                    <div className='mt-2'>As of now you can not log into BGMeetups using third parties. The links are there as placeholders.</div>
                 </li>
-                <li className='mt-2 list-group-item'>
-                    <h4>Users can delete their messages</h4>
+                <li className='mt-3 list-group-item'>
+                    <h4>Suggestions / Bug Report</h4>
+                    <div className='mt-2'>I plan on setting up a system to send suggestions and report a bug. Only verified emails will be accepted.</div>
                 </li>
-                <li className='mt-2 list-group-item'>
-                    <h4>Users can reply to received messages</h4>
+                <li className='mt-3 list-group-item'>
+                    <h4>Group Privacy, Reputation System, and Friends List</h4>
+                    <div className='mt-2'>You will be able to set a group to Private user's can not request an invite. A user's reputation lowers if they miss a meetup along with other factors. A user's reputation will not change for missing a </div>
                 </li>
-            </ul>
-
-            <h3 className='mt-5'>Future changes / Features</h3>
-            <ul className='mt-3 list-group list-group-flush'>
                 <li className='mt-3 list-group-item'>
                     <h4>MarketPlace Functionality</h4>
-                    <div className='mt-2'>I may decide to drop this feature. BGG has a marketplace where people sell board games and if you want new games then there are plenty of awesome choices. Coolstuffinc, the BGG store, and MinMarket are my personal choices</div>
-                </li>
-                <li className='mt-2 list-group-item'>
-                    <h4>Web Camera Functionality</h4>
-                    <div>This is the feature that started the idea of this application. The idea is to be able to play games virtually with members of your group with only one person needing to own the game</div>
-                </li>
-            </ul>
-
-
-            <h3 className='mt-5'>Technical Updates</h3>
-            <ul className='mt-3 list-group list-group-flush'>
-                <li className='mt-3 list-group-item'>
-                    <h4>More Test Coverage</h4>
-                    <div className='mt-2'>I know some programmers do not like writing tests but I beleive there should always be tests. Having some coverage is better than no coverage.</div>
-                </li>
-                <li className='mt-2 list-group-item'>
-                    <h4>Routing</h4>
-                    <div>I am planning changes to the routes and the URL names. The back end is RESTful so I'm thinking of using it for the front end</div>
-                </li>
-                <li className='mt-2 list-group-item'>
-                    <h4>File Structure</h4>
-                    <div>React is not as opinionated as Rails so I need to look up best practices etc. in order to better structure the app.</div>
-                </li>
-                <li className='mt-2 list-group-item'>
-                    <h4>Refactor </h4>
-                    <div>The React side of this application needs to be refactored. I always try to program using best practices or program in that programming languages style. Sometimes I do have to just put something out, even if it's dated or inneficient. </div>
+                    <div className='mt-2'>I plan on setting up a Math Trade system based on region. Whenever someone puts up a game in your region that you want and you have a game they want, you will be notified via a message.</div>
                 </li>
             </ul>
         </div>
