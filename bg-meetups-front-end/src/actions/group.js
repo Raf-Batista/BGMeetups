c8d184a7e8a3595dcb6af24e983744bea725c1c1
Rafael Batista <RafaelB2424@gmail.com>
2020-08-24 15:10:33 -0400

    Add edit group request, success, and failure action creators

--- bg-meetups-front-end/src/actions/group.js
+++ bg-meetups-front-end/src/actions/group.js
@@ -13,3 +13,19 @@ export const createGroupFailure = (error) => ({
   type: types.CREATE_GROUP_FAILURE,
   payload: error,
 });
+
+export const editGroupRequest = () => ({
+  type: types.EDIT_GROUP_REQUEST,
+});
+
+export const editGroupSuccess = (group) => ({
+  type: types.EDIT_GROUP_SUCCESS,
+  payload: group,
+});
+
+export const editGroupFailure = (error) => ({
+  type: types.EDIT_GROUP_FAILURE,
+  payload: error,
+});
+
+
