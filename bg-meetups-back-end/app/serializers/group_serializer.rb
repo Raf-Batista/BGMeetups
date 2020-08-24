3b00f5519dc1b0e3af13978aa0f7628d3598d864
Rafael Batista <RafaelB2424@gmail.com>
2020-08-24 14:54:18 -0400

    Add memberships method that returns only the username of the members

--- bg-meetups-back-end/app/serializers/group_serializer.rb
+++ bg-meetups-back-end/app/serializers/group_serializer.rb
@@ -1,3 +1,11 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :purpose
+  attributes :id, :name, :purpose, :memberships, :owner
+
+  def memberships 
+    object.memberships.map do |membership|
+      user = User.find_by(id: membership.user_id)
+      {username: user.username}
+    end 
+  end 
+  
 end
