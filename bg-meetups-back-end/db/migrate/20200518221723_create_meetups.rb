a8b20f4bc066f63b0274d2f18488ef3be8718889
Rafael Batista <RafaelB2424@gmail.com>
2020-08-24 15:00:45 -0400

    Change integer to belongs_to

--- bg-meetups-back-end/db/migrate/20200518221723_create_meetups.rb
+++ bg-meetups-back-end/db/migrate/20200518221723_create_meetups.rb
@@ -1,7 +1,7 @@
 class CreateMeetups < ActiveRecord::Migration[6.0]
   def change
     create_table :meetups do |t|
-      t.integer :group_id
+      t.belongs_to :group
       t.datetime :meetup_time
       t.integer :attended, array: true, default: []
       t.integer :no_show, array: true, default: []
