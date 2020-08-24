6426d9ddb0f4ceddb5e4f63daf476fb581d61ecf
Rafael Batista <RafaelB2424@gmail.com>
2020-08-24 15:03:12 -0400

    Change ineger to belongs to

--- bg-meetups-back-end/db/migrate/20200812155609_create_received_messages.rb
+++ bg-meetups-back-end/db/migrate/20200812155609_create_received_messages.rb
@@ -1,7 +1,7 @@
 class CreateReceivedMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :received_messages do |t|
-      t.integer :user_id
+      t.belongs_to :user
       t.string :from
       t.string :subject
       t.text :content
