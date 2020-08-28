7384f3c66218f9fa9b309d817fa55a7932535df1
Rafael Batista <RafaelB2424@gmail.com>
2020-08-28 14:33:58 -0400

    Create Invitation model which belongs to a Group with validations, add method to get all of a user's invitations and the group name

---
+++ bg-meetups-back-end/app/models/invitation.rb
@@ -0,0 +1,18 @@
+class Invitation < ApplicationRecord
+    belongs_to :group 
+    validates :from, presence: true 
+    validates :recipient, presence: true 
+    validates :invitation_link, presence: true
+
+    def self.get_invitations(user) 
+        invitations = Invitation.all 
+
+        invitations.select do |invitation| 
+            invitation.recipient == user && invitation_link.active
+        end 
+    end 
+
+    def group_name 
+        self.group.name
+    end 
+end
