5ecc2885343c2a3787b8ca75da1811202ad6d467
Rafael Batista <RafaelB2424@gmail.com>
2020-08-28 15:34:59 -0400

    Create action creators for sending and receiving invitations

---
+++ bg-meetups-front-end/src/actions/invitation.js
@@ -0,0 +1,29 @@
+import * as types from '../constants/invitation';
+
+export const sentInvitationRequest = () => ({
+    type: types.SENT_INVITATION_REQUEST, 
+})
+
+export const receivedInvitationSuccess = (invitation) => ({
+    type: types.RECEIVED_INVITATION_SUCCESS, 
+    payload: invitation
+});
+
+export const sentInvitationFailure = (error) => ({
+    type: types.SENT_INVITATION_REQUEST, 
+    payload: error
+});
+
+export const fetchInvitationsRequest = () => ({
+    type: types.FETCH_INVITATIONS_REQUEST
+})
+
+export const fetchInvitationsSuccess = (invitations) => ({
+    type: types.FETCH_INVITATIONS_SUCCESS,
+    payload: invitations
+})
+
+export const fetchInvitationsFailure = (error) => ({
+    type: types.FETCH_INVITATIONS_FAILURE, 
+    payload: error
+})
