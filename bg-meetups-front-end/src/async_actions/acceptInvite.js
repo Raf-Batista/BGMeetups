5057362b33274cf3d41f95aa18ea7ba9cfb647e7
Rafael Batista <RafaelB2424@gmail.com>
2020-09-01 15:48:05 -0400

    Add fetch request and dispatch appropriate actions when accepting an invitation

--- bg-meetups-front-end/src/async_actions/acceptInvite.js
+++ bg-meetups-front-end/src/async_actions/acceptInvite.js
@@ -1,6 +1,30 @@
-const acceptInvite = (invite) => {
-    return async (dispatch) => {
+import * as actions from "../actions/invitation";
+import { toast } from "react-toastify";
 
+const acceptInvite = (id, inviteLink) => {
+    return async (dispatch) => {
+        try {
+            dispatch(actions.acceptInvitationRequest())
+            const response = await fetch(inviteLink, {
+                credentials: "include",
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Accept: "application/json",
+                },
+            })
+
+            const result = await response.json();
+
+            if(!result.error) {
+                toast.success("Accepted Invite", {
+                    position: toast.POSITION.TOP_CENTER,
+                  });
+                return dispatch(actions.acceptInvitationSuccess(result))
+            }
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
