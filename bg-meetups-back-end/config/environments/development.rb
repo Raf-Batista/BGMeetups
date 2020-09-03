f006a1d768ba58f88cb684a44e5589f25d992e29
Rafael Batista <RafaelB2424@gmail.com>
2020-09-03 17:40:44 -0400

    Config cache store to use redis

--- bg-meetups-back-end/config/environments/development.rb
+++ bg-meetups-back-end/config/environments/development.rb
@@ -22,7 +22,8 @@ Rails.application.configure do
   else
     config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
+  # Use Redis store for caching.
+  config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL_CACHING", "redis://localhost:6379/0") }
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
