a81ec1e5db62189208ab6d602d8a0ded1fc567b6
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:36:52 -0400

    Change styling for messages

--- bg-meetups-front-end/src/components/Messages.js
+++ bg-meetups-front-end/src/components/Messages.js
@@ -1,49 +1,46 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Message from './Message';
-import SentMessage from './SentMessage';  
+import SentMessage from './SentMessage';
 
 const Messages = () => {
-    const [active, setActive] = useState(true);
-    /* The '|| []' will prevent an error when refreshing the page. sentMessages and receivedMessages will be undefined on refresh without it */
-    const sentMessages = useSelector(state => state.user.sent_messages) || [];
-    const receivedMessages = useSelector(state => state.user.received_messages) || [];
+  const [active, setActive] = useState(true);
+  /* The '|| []' will prevent an error when refreshing the page. sentMessages and receivedMessages will be undefined on refresh without it */
+  const sentMessages = useSelector(state => state.user.sent_messages) || [];
+  const receivedMessages = useSelector(state => state.user.received_messages) || [];
 
-    const handleClick = () => {
-        setActive(!active);
-      };
+  const handleClick = () => {
+    setActive(!active);
+  };
 
-    return (
-        <div className='container messages'>
-          <div className='row'>
-            
-            <div className='col-6 text-justify'>
-                    <div
-                      className={`pointer ${active ? "message-selection" : ""}`}
-                      onClick={handleClick}
-                    >
-                    Recieved
-                    </div>
-                  <div
-                    className={`pointer mt-2 ${!active ? "message-selection" : ""}`}
-                    onClick={handleClick}
-                  >
-                    Sent
-                  </div>
-            </div>
+  return (
+    <div className='container messages'>
+      <div className='text-justify-right div-background py-2'>
+        <span
+          className={`pointer ${active ? "message-selection" : ""}`}
+          onClick={handleClick}
+        >
+          Recieved
+        </span>
+        <span
+          className={`pointer mt-2 ml-4 ${!active ? "message-selection" : ""}`}
+          onClick={handleClick}
+        >
+          Sent
+        </span>
+      </div>
 
-            <div className='col-6 mt-2'>
-              {
-                active ? 
-               
-                receivedMessages.map((receivedMessage) => <Message key={receivedMessage.id} message=  {receivedMessage} />) :
-                
-                sentMessages.map((sentMessage) => <Message message={sentMessage} />)
-              }
-            </div>
-          </div> 
-        </div>
-    )
+      <div>
+        {
+          active ?
+
+            receivedMessages.map((receivedMessage) => <Message key={receivedMessage.id} message={receivedMessage} />) :
+
+            sentMessages.map((sentMessage) => <Message key={sentMessage.id} message={sentMessage} />)
+        }
+      </div>
+    </div>
+  )
 }
 
 export default Messages
