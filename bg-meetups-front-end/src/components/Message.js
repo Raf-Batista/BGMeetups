b13b1cc8510e7838d79f9d0505363838eda1551b
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:35:42 -0400

    Add background, padding, and style to buttons

--- bg-meetups-front-end/src/components/Message.js
+++ bg-meetups-front-end/src/components/Message.js
@@ -1,32 +1,31 @@
 import React, { useState } from 'react'
 
 const Message = (props) => {
-    const {message} = props;
+    const { message } = props;
     const [show, setShow] = useState(false);
     const handleClick = () => {
         setShow(!show);
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log('AAARGH')
     }
 
     return (
-        <div className='mb-4 text-left'>
-           {
-               !show ? <h6 className='pointer message-subjects mb-3' onClick={handleClick}>Subject: {message.subject}</h6> : null
-           } 
+        <div className='text-center div-background'>
             {
-             show ? 
-                <div className='message-body'>
-                   <div>From: {message.from}</div>
-                   <div>Subject: {message.subject}</div>
-                   <div className='message-content mt-1'>{message.content}</div> 
-                   <form  className='mt-3 d-inline' onSubmit={handleSubmit}>
-                     <button className='btn message-delete-btn'>Delete</button>
-                    </form>
-                    <button className='btn d-inline' onClick={handleClick}>Close</button>
-                </div> : null
+                !show ? <h6 className='pointer mb-3 pt-3' onClick={handleClick}>Subject: {message.subject}</h6> : null
+            }
+            {
+                show ?
+                    <div className='message-body mb-3 py-2'>
+                        <div>From: {message.from}</div>
+                        <div>Subject: {message.subject}</div>
+                        <div className='message-content mt-1'>{message.content}</div>
+                        <form className='mt-3 d-inline' onSubmit={handleSubmit}>
+                            <button className='small-btn px-2 message-delete-btn mt-2'>Delete</button>
+                        </form>
+                        <button className=' ml-2 small-btn px-2 d-inline mt-2' onClick={handleClick}>Close</button>
+                    </div> : null
             }
         </div>
     )
