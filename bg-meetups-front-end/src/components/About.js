cc873646477fffa890de12bb36ce1fe0278bab36
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:10:23 -0400

    Create about page

--- bg-meetups-front-end/src/components/About.js
+++ bg-meetups-front-end/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
     return (
         <div className='container mt-5 text-center'>
-            ABOUT
+            About Page
         </div>
     )
 }
