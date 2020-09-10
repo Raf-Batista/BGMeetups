560419393db3969f92ea3b2a3227c7bbff97cf1b
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:37:36 -0400

    Add div background, padding, and style to buttons

--- bg-meetups-front-end/src/components/Private.js
+++ bg-meetups-front-end/src/components/Private.js
@@ -32,9 +32,9 @@ const Private = (props) => {
   };
 
   const form = (
-    <form className="mt-4 mx-lg-auto ml-sm-4" onSubmit={handleSubmit}>
+    <form className="mt-4" onSubmit={handleSubmit}>
       <div className="form-group">
-        <label className="d-block my-2">Email Address</label>
+        <label className="d-block mt-2">Email Address</label>
         <input
           name="email"
           onChange={handleChange}
@@ -42,7 +42,7 @@ const Private = (props) => {
           placeholder={user.email}
           value={userForm.email}
         />
-        <label className="d-block my-2">Password</label>
+        <label className="d-block mt-3">Password</label>
         <input
           name="password"
           onChange={handleChange}
@@ -50,15 +50,15 @@ const Private = (props) => {
           placeholder={"*******"}
           value={userForm.password}
         />
-        <div>
+        <div className="mt-2 mb-5">
           <button
-            className="d-inline mx-2 mt-2 border-0 bg-transparent"
+            className="d-inline mx-2 small-btn px-3 save-btn"
             type="submit"
           >
             Save
           </button>
           <button
-            className="d-inline border-0 bg-transparent"
+            className="d-inline small-btn"
             onClick={handleClick}
           >
             Cancel
@@ -71,18 +71,18 @@ const Private = (props) => {
   const privateInfo = (
     <div>
       <div className="mt-4">Email Address</div>
-      <div className="mt-2">{user.email}</div>
-      <div className="mt-2">Password</div>
-      <div className="mt-2">**********</div>
-      <button className="pointer border-0 bg-transparent" onClick={handleClick}>
+      <div className="mt-3">{user.email}</div>
+      <div className="mt-3">Password</div>
+      <div className="mt-3">**********</div>
+      <button className="pointer small-btn px-3 mt-1 mb-5" onClick={handleClick}>
         Edit
       </button>
     </div>
   );
 
   return (
-    <div>
-      <span className="d-block">
+    <div className="div-background account">
+      <span className="d-block p-3">
         This is your account information. Only visible to you.
       </span>
       <span className="d-block">
