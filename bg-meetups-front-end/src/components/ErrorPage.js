97c92a87e96d4403820e92ab906c6debc84db761
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:27:32 -0400

    Create error page with a button to go back

---
+++ bg-meetups-front-end/src/components/ErrorPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const ErrorPage = (props) => {
+    const { history } = props;
+    const handleClick = () => {
+        history.goBack();
+    };
+
+    return (
+        <div className="container text-center mt-5">
+            <div>
+                The page you reqeuested is not found
+            </div>
+            <div className="mt-3">
+                Click here to go back
+            </div>
+            <div className="mt-3">
+                <button className="small-btn" onClick={handleClick}>Go Back</button>
+            </div>
+        </div>
+    )
+}
+
+export default ErrorPage
