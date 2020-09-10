1ac2ea208dd9e61aae7fbcd15972583f8d390ec7
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:39:40 -0400

    Add div background, padding, and style to buttons

--- bg-meetups-front-end/src/containers/MarketEditContainer.js
+++ bg-meetups-front-end/src/containers/MarketEditContainer.js
@@ -32,7 +32,7 @@ const MarketEditContainer = (props) => {
           sell a game
         </span>
       </div>
-      <ul className="nav justify-content-center my-4">
+      <ul className="nav justify-content-center mt-4 div-background py-2">
         <li className="nav-item">
           <div
             className={`nav-link pointer ${active ? "active" : ""}`}
