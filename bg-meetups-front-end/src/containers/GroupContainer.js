a584f3ee1af99c931cb8bb1b6223906bbf89f8bf
Rafael Batista <RafaelB2424@gmail.com>
2020-09-09 20:38:39 -0400

    Fix spacing

--- bg-meetups-front-end/src/containers/GroupContainer.js
+++ bg-meetups-front-end/src/containers/GroupContainer.js
@@ -4,14 +4,14 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Groups from "../components/Groups";
 
-const GroupContainer = () => {    
+const GroupContainer = () => {
   const [query, setQuery] = useState('');
   const lowercasedQuery = query.toLowerCase();
   const groups = useSelector(state => state.groups);
   const handleChange = (e) => {
-      setQuery(e.target.value);
+    setQuery(e.target.value);
   }
-  const filteredGroups = groups.filter((group) => { 
+  const filteredGroups = groups.filter((group) => {
     if (group.name.toLowerCase().includes(lowercasedQuery)) {
       return group;
     }
@@ -19,19 +19,23 @@ const GroupContainer = () => {
 
   return (
     <div className='container mt-5 text-center'>
-        <div className='d-inline'> 
-            <form className=' d-inline'>
-                <input type='text' name='query' placeholder="Search for groups by name." onChange={handleChange} className='pl-2'/>
-                {/* <span className='input-group-addon'>
+      <div className="heading">
+        <h2 className="d-block my-3">Groups</h2>
+        <span>Search through a list of all board game groups</span>
+      </div>
+      <div className='d-inline'>
+        <form className=' d-inline'>
+          <input type='text' name='query' placeholder="Search" onChange={handleChange} className='pl-2 my-2' />
+          {/* <span className='input-group-addon'>
                     <FontAwesomeIcon
                     icon={faSearch}
                     size="lg"
                     className=''
                     />
                 </span> */}
-            </form>
-        </div>
-        <Groups groups={filteredGroups} />
+        </form>
+      </div>
+      <Groups groups={filteredGroups} />
     </div>
   );
 };
