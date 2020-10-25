b46e440e5ac3ec942dea5f8b8d1081aa51dfd59a
Rafael Batista <RafaelB2424@gmail.com>
2020-10-25 14:13:47 -0400

    Update

--- bg-meetups-front-end/src/components/Market/index.js
+++ bg-meetups-front-end/src/components/Market/index.js
@@ -1,4 +1,4 @@
 import MarketContainer from './MarketContainer';
-import MarketEditContainer from './MarketContainer';
+import MarketEditContainer from './MarketEditContainer';
 
 export { MarketContainer, MarketEditContainer };
