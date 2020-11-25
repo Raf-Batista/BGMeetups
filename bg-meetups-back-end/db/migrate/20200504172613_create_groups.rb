e80232f67bd4493b4b7425cd926cfbd97903f362
Rafael Batista <RafaelB2424@gmail.com>
2020-11-25 14:31:48 -0500

    Add required validation for belongs to association

--- bg-meetups-back-end/db/migrate/20200504172613_create_groups.rb
+++ bg-meetups-back-end/db/migrate/20200504172613_create_groups.rb
@@ -2,7 +2,7 @@ class CreateGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :groups do |t|
      # t.references :user, foreign_key: true
-      t.belongs_to :user
+      t.belongs_to :user, required: true
       t.string :status, default: "open"
       t.string :purpose 
       t.string :name
