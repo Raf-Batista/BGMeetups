da30e00cf0bb6260122aa511d0fe647cfe45f65c
Raf-Batista <RafaelB2424@gmail.com>
2020-11-30 17:36:45 -0800

    Refactor

--- bg-meetups-front-end/src/components/Groups/ManageGroupMembers/ManageGroupMembers.js
+++ bg-meetups-front-end/src/components/Groups/ManageGroupMembers/ManageGroupMembers.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import Member from '../GroupMember';
 import InvitationForm from '../GroupInvitationForm';
+import './ManageGroupMembers.css';
+import MeetupForm from '../../Meetups/MeetupForm';
 
 const ManageGroupMembers = (props) => {
     let { members, history, userId, groupId, meetups } = props;
-    members = !!members ? members : []
-    meetups = !!meetups ? meetups : []
+    members = !!members ? members : [];
+    meetups = !!meetups ? meetups : [];
     const [showForm, setShowForm] = useState(false);
+
     const handleClick = () => {
         setShowForm(!showForm)
     };
@@ -14,20 +17,23 @@ const ManageGroupMembers = (props) => {
     return (
         <div className='container'>
             <h4>Members</h4>
+
             {showForm ?
-                <div>
+                <>
                     <InvitationForm cancel={handleClick} history={history} userId={userId} groupId={groupId} />
-                </div> :
-                <div>
+                </> :
+
+                <>
                     <h6 className='pointer invite-link' onClick={handleClick}>Invite</h6>
                     {members.map(member => <Member member={member} />)}
-                </div>
+                </>
             }
-            <h4 className="my-2">Meetups</h4>
-            <h6 className='pointer invite-link' onClick={handleClick}>Create Meetup</h6>
+
+            <h4 className="my-2">Meetups</h4>            
+            <MeetupForm />
             {meetups.map(meetup => meetup.meetup_time)}
         </div>
     )
 }
 
-export default ManageGroupMembers
+export default ManageGroupMembers;
