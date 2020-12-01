27c757ed8658132edd8a611b57c936c28dce75c4
Raf-Batista <RafaelB2424@gmail.com>
2020-11-30 17:37:03 -0800

    Create meetup form

--- bg-meetups-front-end/src/components/Meetups/MeetupForm/MeetupForm.js
+++ bg-meetups-front-end/src/components/Meetups/MeetupForm/MeetupForm.js
@@ -1,10 +1,26 @@
-import React from 'react'; 
+import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import useForm from "../../../hooks/useForm";
 
 const MeetupForm = () => {
+    const dispatch = useDispatch();
+    const initialDateState = {date: Date.now()};
+
+    const submit = () => {
+        //dispatch(values)
+        console.log(date)
+    }
+
+    const {handleSubmit, handleChange, values} = useForm(initialDateState, submit);
+    const date = values;
+
     return (
-        <div>
-            Meetup Form
-        </div>
+        <>
+            <form onSubmit={handleSubmit} className="meetup-form">
+                <input type="datetime-local" onChange={handleChange} value={date}/>
+                <button type="submit" className="ml-2 btn meetup-btn">Create</button>
+            </form>
+        </>
     )
 }
 
