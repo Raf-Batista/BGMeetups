411b32e0c79d15684a85d9e5f65a990eee382c94
Rafael Batista <RafaelB2424@gmail.com>
2020-12-15 21:54:55 -0500

    Create sidenav component with markup

---
+++ bg-meetups-front-end/src/components/Sidenav/Sidenav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Sidenav.scss';
+
+const Sidenav = () => {
+    return (
+        <section className="section-sidenav">
+            <div className="sidenav__toggle">
+                &nbsp;
+            </div>
+            <div className="sidenav__logo">
+                LOGO
+            </div>
+        </section>
+    )
+}
+
+export default Sidenav
