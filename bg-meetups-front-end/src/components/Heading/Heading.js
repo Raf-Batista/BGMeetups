ca26640e3f9a87e98c9b74245b361860dbea1b38
Rafael Batista <RafaelB2424@gmail.com>
2020-12-15 21:54:21 -0500

    Comment out code

--- bg-meetups-front-end/src/components/Heading/Heading.js
+++ bg-meetups-front-end/src/components/Heading/Heading.js
@@ -46,98 +46,103 @@ const Heading = () => {
     </li>
   </ul>
   return (
-    <div className='logo'>
-      <div className='d-inline'>
-        {/* <img src={dice} /> */}
-      </div>
-
-      <div className='d-inline'>
-        <small className='logo-name'>BoardGame Meetups</small>
-      </div>
-
-      <div className='d-inline pointer' onClick={handleClick} >
-        <span className='user-account-text'>Account</span>
-
-        {/* <FontAwesomeIcon
-          icon={faUser}
-          size="lg"
-          className='user-account-icon'
-        /> */}
-      </div>
-
-      <div className='sidenav' style={open ? { width: '250px' } : { width: '0' }}>
-        <a href="#" className="closebtn" onClick={handleClick} >&times;</a>
-        {
-          JSON.stringify(currentUser) === "{}" ?
-            loginNav :
-            <ul>
-              <li onClick={handleClick}>
-                <NavLink
-                  exact
-                  className="nav-link"
-                  to="/account"
-                  data-test="link-navbar">
-                  Account
-                          </NavLink>
-              </li>
-
-              <li onClick={handleClick}>
-                <NavLink
-                  exact
-                  className="nav-link"
-                  to="/my-market"
-                  data-test="link-navbar">
-                  My Market
-                          </NavLink>
-              </li>
-
-              <li onClick={handleClick}>
-                <NavLink
-                  exact
-                  className="nav-link"
-                  to={`/${currentUser.id}/groups`}
-                  data-test="link-navbar">
-                  My Groups
-                          </NavLink>
-              </li>
-
-              <li onClick={handleClick}>
-                <NavLink
-                  exact
-                  className="nav-link"
-                  to="/messages"
-                  data-test="link-navbar">
-                  Messages
-                          </NavLink>
-              </li>
-
-              <li onClick={handleClick}>
-                <NavLink
-                  exact
-                  className="nav-link"
-                  to="/invites"
-                  data-test="link-navbar">
-                  Invites
-                          </NavLink>
-              </li>
-
-              <li onClick={logout}>
-                <NavLink
-                  exact
-                  className="nav-link"
-                  to="/logout"
-                  data-test="link-navbar">
-                  Logout
-                          </NavLink>
-              </li>
-            </ul>
-        }
-        <li onClick={handleClick}>
-
-        </li>
-      </div>
-    </div>
+    <>
+    HEADING!!
+    </>
   )
-}
+//   return (
+//     <div className='logo'>
+//       <div className='d-inline'>
+//         {/* <img src={dice} /> */}
+//       </div>
 
+//       <div className='d-inline'>
+//         <small className='logo-name'>BoardGame Meetups</small>
+//       </div>
+
+//       <div className='d-inline pointer' onClick={handleClick} >
+//         <span className='user-account-text'>Account</span>
+
+//         {/* <FontAwesomeIcon
+//           icon={faUser}
+//           size="lg"
+//           className='user-account-icon'
+//         /> */}
+//       </div>
+
+//       <div className='sidenav' style={open ? { width: '250px' } : { width: '0' }}>
+//         <a href="#" className="closebtn" onClick={handleClick} >&times;</a>
+//         {
+//           JSON.stringify(currentUser) === "{}" ?
+//             loginNav :
+//             <ul>
+//               <li onClick={handleClick}>
+//                 <NavLink
+//                   exact
+//                   className="nav-link"
+//                   to="/account"
+//                   data-test="link-navbar">
+//                   Account
+//                           </NavLink>
+//               </li>
+
+//               <li onClick={handleClick}>
+//                 <NavLink
+//                   exact
+//                   className="nav-link"
+//                   to="/my-market"
+//                   data-test="link-navbar">
+//                   My Market
+//                           </NavLink>
+//               </li>
+
+//               <li onClick={handleClick}>
+//                 <NavLink
+//                   exact
+//                   className="nav-link"
+//                   to={`/${currentUser.id}/groups`}
+//                   data-test="link-navbar">
+//                   My Groups
+//                           </NavLink>
+//               </li>
+
+//               <li onClick={handleClick}>
+//                 <NavLink
+//                   exact
+//                   className="nav-link"
+//                   to="/messages"
+//                   data-test="link-navbar">
+//                   Messages
+//                           </NavLink>
+//               </li>
+
+//               <li onClick={handleClick}>
+//                 <NavLink
+//                   exact
+//                   className="nav-link"
+//                   to="/invites"
+//                   data-test="link-navbar">
+//                   Invites
+//                           </NavLink>
+//               </li>
+
+//               <li onClick={logout}>
+//                 <NavLink
+//                   exact
+//                   className="nav-link"
+//                   to="/logout"
+//                   data-test="link-navbar">
+//                   Logout
+//                           </NavLink>
+//               </li>
+//             </ul>
+//         }
+//         <li onClick={handleClick}>
+
+//         </li>
+//       </div>
+//     </div>
+//   )
+// }
+}
 export default Heading
