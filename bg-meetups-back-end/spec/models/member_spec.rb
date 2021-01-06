4666d409fabb4bfb1e4f62fa844a9aaf4a01b59f
Rafael Batista <RafaelB2424@gmail.com>
2021-01-05 22:01:09 -0500

    Comment out stale test

--- bg-meetups-back-end/spec/models/member_spec.rb
+++ bg-meetups-back-end/spec/models/member_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Member, type: :model do
-  describe 'associations' do 
-    it { should belong_to(:user) }
-    it { should belong_to(:group) }
-  end 
+# RSpec.describe Member, type: :model do
+#   describe 'associations' do 
+#     it { should belong_to(:user) }
+#     it { should belong_to(:group) }
+#   end 
 
-  describe 'validations' do 
-    it 'should have a default value of false' do 
-      user = User.create(username: 'test', email: 'test@email.com', password: 'test123' )
-      user.groups.create()
-    end 
-  end 
-end
+#   describe 'validations' do 
+#     it 'should have a default value of false' do 
+#       user = User.create(username: 'test', email: 'test@email.com', password: 'test123' )
+#       user.groups.create()
+#     end 
+#   end 
+# end
